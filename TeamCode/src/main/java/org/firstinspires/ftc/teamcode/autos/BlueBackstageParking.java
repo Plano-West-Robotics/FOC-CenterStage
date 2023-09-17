01caa747956da715d0297007baaa2f03c2b5488a
Yash Karandikar <nerdstep710@gmail.com>
2023-09-16 20:07:38 -0500

    Backstage autos

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/BlueBackstageParking.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.autos;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.inchworm.InchWorm;
+import org.firstinspires.ftc.teamcode.inchworm.WormUtil;
+import org.firstinspires.ftc.teamcode.inchworm.units.Angle;
+import org.firstinspires.ftc.teamcode.inchworm.units.Distance;
+
+@Autonomous
+public class BlueBackstageParking extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        WormUtil wormUtil = new WormUtil(this, InchWorm.GLOBAL_ORIENTATION);
+        InchWorm inchWorm = new InchWorm(this, InchWorm.GLOBAL_ORIENTATION, InchWorm.POSE_ZERO);
+
+        wormUtil.waitForStart();
+
+        inchWorm.moveTo(new InchWorm.Pose(Distance.ZERO, Distance.tiles(-4), Angle.ZERO));
+    }
+}
