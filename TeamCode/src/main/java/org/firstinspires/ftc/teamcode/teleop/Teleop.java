c1b0895934a0f9ec54146205a842f24582d9b199
Yash Karandikar <nerdstep710@gmail.com>
2023-10-02 12:27:26 -0500

    Make basic teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Teleop.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.subsystems.Drive;
+
+@TeleOp(name = "hopefully working teleop")
+public class Teleop extends OpMode {
+    Hardware hardware;
+    Drive drive;
+
+    @Override
+    public void init() {
+        hardware = new Hardware(this);
+        drive = new Drive(hardware);
+
+        drive.setSpeed(0.5);
+        drive.setFieldOriented(false);
+    }
+
+    @Override
+    public void loop() {
+        drive.drive(gamepad1.left_stick_x, -gamepad1.left_stick_y, gamepad2.left_stick_x);
+    }
+}
