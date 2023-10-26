873fe5741df4e7a5ea2e73af3d3beb2a1124e35c
Yash Karandikar <nerdstep710@gmail.com>
2023-10-26 17:00:49 -0500

    Decouple MotorTester from Hardware (helps when the hub is detached from the bot)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MotorTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MotorTester.java
@@ -1,14 +1,18 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-import org.firstinspires.ftc.teamcode.OpModeWrapper;
-import org.firstinspires.ftc.teamcode.subsystems.GamepadWrapper;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(group="test")
-public class MotorTester extends OpModeWrapper {
+public class MotorTester extends OpMode {
+    DcMotor intake;
     @Override
-    public void run() {
-        hardware.intake.setPower(gamepad1.getAnalogValue(GamepadWrapper.AnalogInput.LEFT_STICK_Y));
+    public void init() {
+        intake = hardwareMap.get(DcMotor.class, "intake");
+    }
+    @Override
+    public void loop() {
+        intake.setPower(-gamepad1.left_stick_y);
     }
 }
