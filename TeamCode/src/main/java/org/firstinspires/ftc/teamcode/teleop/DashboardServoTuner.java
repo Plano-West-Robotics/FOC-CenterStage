5edd7ed15edbcdc3f11b99c219353762b1aef221
missing <piecenotfound@gmail.com>
2023-11-10 22:32:51 -0600

    Update ServoTuner to work with multiple servos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/DashboardServoTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/DashboardServoTuner.java
@@ -1,66 +1,57 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
 import com.acmerobotics.dashboard.FtcDashboard;
-import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.config.ValueProvider;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ServoImplEx;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
-
-@Config
 @TeleOp
 public class DashboardServoTuner extends OpMode {
-    public static double servoPos = 0.5;
-
-    ServoImplEx servo = null;
-    String servoName = "";
-
     @Override
     public void init() {
         FtcDashboard db = FtcDashboard.getInstance();
 
-        Telemetry telemetry = db.getTelemetry();
-        telemetry.addLine("The following servos are available:");
-        for (String name : hardwareMap.getAllNames(ServoImplEx.class)) {
-            telemetry.addLine(name);
+        for (ServoImplEx s : hardwareMap.getAll(ServoImplEx.class)) {
+            String name = hardwareMap.getNamesOf(s).iterator().next();
+            if (name == null) continue;
+
+            db.addConfigVariable(this.getClass().getSimpleName(), name, new ValueProvider<String>() {
+                final ServoImplEx servo = s;
+                double pos = 0.5;
+                String verbatimInput = "";
+                boolean isActive = false;
+
+                @Override
+                public String get() {
+                    return verbatimInput;
+                }
+
+                @Override
+                public void set(String value) {
+                    if (value.equals("")) {
+                        this.isActive = false;
+                        this.servo.setPwmDisable();
+                        this.verbatimInput = value;
+                        return;
+                    }
+
+                    try {
+                        this.pos = Double.parseDouble(value);
+                    } catch (NumberFormatException e) {
+                        // do not acknowledge the request
+                        return;
+                    }
+
+                    this.isActive = true;
+                    this.servo.setPwmEnable();
+                    this.servo.setPosition(this.pos);
+                    this.verbatimInput = value;
+                }
+            });
         }
-        telemetry.update();
-
-        db.addConfigVariable(this.getClass().getSimpleName(), "servoName", new ValueProvider<String>() {
-            @Override
-            public String get() {
-                return servoName;
-            }
-
-            @Override
-            public void set(String value) {
-                if (hardwareMap.getNamesOf(servo).contains(value)) return;
-
-                ServoImplEx oldServo = servo;
-
-                if (value.equals("")) {
-                    servo = null;
-                } else {
-                    // "you should ONLY call this method during the Init phase of your OpMode" 🤓
-                    ServoImplEx maybeServo = hardwareMap.tryGet(ServoImplEx.class, value);
-                    if (maybeServo == null) return;
-                    servo = maybeServo;
-                }
-                servoName = value;
-
-                if (oldServo != null) {
-                    oldServo.setPwmDisable();
-                }
-            }
-        });
     }
 
     @Override
-    public void loop() {
-        if (servo != null) {
-            servo.setPosition(servoPos);
-        }
-    }
+    public void loop() {}
 }
