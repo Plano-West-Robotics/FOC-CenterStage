ce7d1a3e7d9e5c7561131f941b8e39a47c1be3de
Yash Karandikar <nerdstep710@gmail.com>
2023-11-11 10:21:15 -0600

    Fix backstage parkings

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/RedBackstageParking.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/RedBackstageParking.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.inchworm.InchWorm;
+import org.firstinspires.ftc.teamcode.inchworm.InchWormBackup;
 import org.firstinspires.ftc.teamcode.inchworm.WormUtil;
 import org.firstinspires.ftc.teamcode.inchworm.units.Angle;
 import org.firstinspires.ftc.teamcode.inchworm.units.Distance;
@@ -13,10 +14,12 @@ public class RedBackstageParking extends LinearOpMode {
     @Override
     public void runOpMode() {
         WormUtil wormUtil = new WormUtil(this, InchWorm.GLOBAL_ORIENTATION);
-        InchWorm inchWorm = new InchWorm(this, InchWorm.GLOBAL_ORIENTATION, InchWorm.POSE_ZERO);
+        InchWormBackup inchWorm = new InchWormBackup(this);
 
         wormUtil.waitForStart();
 
-        inchWorm.moveTo(new InchWorm.Pose(Distance.tiles(-4), Distance.ZERO, Angle.ZERO));
+//        inchWorm.moveTo(new InchWorm.Pose(Distance.tiles(-4), Distance.ZERO, Angle.ZERO));
+        inchWorm.moveTo(0, 5);
+        inchWorm.moveTo(-48, 5);
     }
 }
