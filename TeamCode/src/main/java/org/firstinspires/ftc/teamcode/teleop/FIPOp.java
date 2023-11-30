2ba74f5246b18e00885948a4d9dc98674d90fc95
Yash Karandikar <nerdstep710@gmail.com>
2023-11-30 16:22:22 -0600

    Add teleop for ripfips

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/FIPOp.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.teamcode.OpModeWrapper;
+import org.firstinspires.ftc.teamcode.subsystems.Drive;
+
+// teleop for ripfips, doesn't have intake or lift or any other subsystems enabled
+
+@TeleOp(name = "RIPFIP TeleOp")
+public class FIPOp extends OpModeWrapper {
+    double driveSpeed;
+    Drive drive;
+    @Override
+    public void setup() {
+        driveSpeed = 0.6;
+        drive = new Drive(this.hardware, driveSpeed);
+        drive.setFieldOriented(false);
+    }
+
+    @Override
+    public void run() {
+        if (gamepads.justPressed(Controls.DRIVE_FASTER)) {
+            driveSpeed += 0.15;
+        }
+        if (gamepads.justPressed(Controls.DRIVE_SLOWER)) {
+            driveSpeed -= 0.15;
+
+        }
+        driveSpeed = Range.clip(driveSpeed, 0.1, 1.0);
+        drive.setSpeed(driveSpeed);
+        telemetry.addData("Drive speed", driveSpeed);
+
+        double y = gamepads.getAnalogValue(Controls.STRAIGHT);
+        double x = gamepads.getAnalogValue(Controls.STRAFE);
+        double turn = gamepads.getAnalogValue(Controls.TURN);
+        drive.drive(x, y, turn);
+    }
+}
