ab62cf62d075f564b2224d42e2aaa19b1100a3b3
gallant <vincentknightwork@gmail.com>
2023-12-05 17:25:01 -0600

    Revert "Refresh freesight tuner, add an opmode for tuning,  and restore dashboard functionality."
    This reverts commit cd7f9201d18854dbea705c27961b3533e09ea905.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -10,6 +10,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
+
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.inchworm.InchWorm;
@@ -17,23 +18,21 @@ import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 
 /**
- * Hardware wrapper to abstract motors and stuff. I recommend you use this to
- * keep yourself sane.
+ * Hardware wrapper to abstract motors and stuff. I recommend you use this to keep yourself sane.
  */
 public class Hardware {
-  public DcMotorEx fl, fr, bl, br;
-  public DcMotorEx intake;
-  public DcMotorEx ramp;
-  public DcMotorEx liftL, liftR;
-  public Servo elbowL, elbowR;
-  public Servo wristL, wristR;
-  public Servo flap;
-  public Servo planeLauncher;
-  public IMU imu;
-  public VoltageSensor voltageSensor;
-  public OpenCvCamera webcam;
+    public DcMotorEx fl, fr, bl, br;
+    public DcMotorEx intake;
+    public DcMotorEx ramp;
+    public DcMotorEx liftL, liftR;
+    public Servo elbowL, elbowR;
+    public Servo wristL, wristR;
+    public Servo flap;
+    public Servo planeLauncher;
+    public IMU imu;
+    public VoltageSensor voltageSensor;
+    public OpenCvCamera webcam;
 
-<<<<<<< HEAD
     public OpMode opMode;
 
     /**
@@ -45,121 +44,38 @@ public class Hardware {
         this.opMode = opMode;
 
         HardwareMap hardwareMap = opMode.hardwareMap;
-=======
-  /**
-   * Initialize hardware wrapper. This constructor reverses motors and all that
-   * for you
-   *
-   * @param opMode Current opmode. Used to grab the HardwareMap
-   */
-  public Hardware(OpMode opMode) {
-    HardwareMap hardwareMap = opMode.hardwareMap;
 
-    opMode.telemetry = new MultipleTelemetry(
-        opMode.telemetry, FtcDashboard.getInstance().getTelemetry());
+        opMode.telemetry = new MultipleTelemetry(opMode.telemetry, FtcDashboard.getInstance().getTelemetry());
 
-    fl = hardwareMap.get(DcMotorEx.class, "frontLeft");
-    fr = hardwareMap.get(DcMotorEx.class, "frontRight");
-    bl = hardwareMap.get(DcMotorEx.class, "backLeft");
-    br = hardwareMap.get(DcMotorEx.class, "backRight");
+        fl = hardwareMap.get(DcMotorEx.class, "frontLeft");
+        fr = hardwareMap.get(DcMotorEx.class, "frontRight");
+        bl = hardwareMap.get(DcMotorEx.class, "backLeft");
+        br = hardwareMap.get(DcMotorEx.class, "backRight");
 
-    fl.setDirection(DcMotorSimple.Direction.REVERSE);
-    fr.setDirection(DcMotorSimple.Direction.FORWARD);
-    bl.setDirection(DcMotorSimple.Direction.REVERSE);
-    br.setDirection(DcMotorSimple.Direction.FORWARD);
+        fl.setDirection(DcMotorSimple.Direction.REVERSE);
+        fr.setDirection(DcMotorSimple.Direction.FORWARD);
+        bl.setDirection(DcMotorSimple.Direction.REVERSE);
+        br.setDirection(DcMotorSimple.Direction.FORWARD);
 
-    fl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-    fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-    bl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-    br.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        br.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-    fl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-    fr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-    bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-    br.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        fl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        fr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        br.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-    fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-    fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-    bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-    br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-    intake = hardwareMap.get(DcMotorEx.class, "intake");
-    //        intake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-    intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        intake = hardwareMap.get(DcMotorEx.class, "intake");
+//        intake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
 
-    // TODO: Change this to a CRServo if needed
-    ramp = hardwareMap.get(DcMotorEx.class, "ramp");
-    // commented out because if encoder isn't plugged in it causes this to run
-    // at full speed
-    //        ramp.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-    ramp.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-
-    liftL = hardwareMap.get(DcMotorEx.class, "liftLeft");
-    liftR = hardwareMap.get(DcMotorEx.class, "liftRight");
-
-    //        liftL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-    //        liftR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-    //        liftL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-    //        liftR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-    liftL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-    liftR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-    liftL.setDirection(DcMotorSimple.Direction.REVERSE);
-    liftR.setDirection(DcMotorSimple.Direction.FORWARD);
-
-    elbowL = hardwareMap.get(Servo.class, "elbowL");
-    elbowR = hardwareMap.get(Servo.class, "elbowR");
-    wristL = hardwareMap.get(Servo.class, "wristL");
-    wristR = hardwareMap.get(Servo.class, "wristR");
-    flap = hardwareMap.get(Servo.class, "flap");
-
-    // TODO: calibrate bounds for the flap
-    /*
-     * Measured 2023-11-29
-     * holds: servos will hold this position for the entirety of the match
-     * elbowL hold: 0.45
-     * elbowR hold: 0.5
-     *
-     *           in   out
-     * wristL: 0.07 - 0.5
-     * wristR: 0.90 - 0.45
-     * */
->>>>>>> 138a0e7 (Refresh freesight tuner, add an opmode for tuning,  and restore dashboard functionality.)
-
-    // no need to scale because they'll be holding position
-    //        elbowL.scaleRange(0.05, 0.5);
-    //        elbowR.scaleRange(0.4, 0.85);
-    //        elbowL.setDirection(Servo.Direction.REVERSE);
-    //        elbowR.setDirection(Servo.Direction.FORWARD);
-
-    wristL.scaleRange(0.07, 0.5);
-    wristR.scaleRange(0.45, 0.9);
-    wristR.setDirection(Servo.Direction.REVERSE);
-    wristL.setDirection(Servo.Direction.FORWARD);
-
-    planeLauncher = hardwareMap.get(Servo.class, "launcher");
-
-    imu = hardwareMap.get(IMU.class, "imu");
-    imu.initialize(new IMU.Parameters(InchWorm.GLOBAL_ORIENTATION));
-
-    voltageSensor = hardwareMap.voltageSensor.iterator().next();
-
-    webcam = OpenCvCameraFactory.getInstance().createWebcam(
-        hardwareMap.get(WebcamName.class, "webcam"));
-  }
-
-  /**
-   * Get yaw in specified unit. Counterclockwise is positive. The zero position
-   * is defined by a call to <code>resetYaw</code>
-   *
-   * @param angleUnit Unit of the returned angle
-   * @return yaw in specified unit
-   */
-  public double getYaw(AngleUnit angleUnit) {
-    return imu.getRobotYawPitchRollAngles().getYaw(angleUnit);
-  }
-
-<<<<<<< HEAD
         // TODO: Change this to a CRServo if needed
         ramp = hardwareMap.get(DcMotorEx.class, "ramp");
         // commented out because if encoder isn't plugged in it causes this to run at full speed
@@ -247,18 +163,4 @@ public class Hardware {
     public double getYaw() {
         return getYaw(AngleUnit.RADIANS);
     }
-=======
-  /**
-   * Reset yaw reading to 0.
-   */
-  public void resetYaw() { imu.resetYaw(); }
-
-  /**
-   * Get yaw in radians. Counterclockwise is positive. The zero position is
-   * defined by a call to <code>resetYaw</code>.
-   *
-   * @return yaw in radians
-   */
-  public double getYaw() { return getYaw(AngleUnit.RADIANS); }
->>>>>>> 138a0e7 (Refresh freesight tuner, add an opmode for tuning,  and restore dashboard functionality.)
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/RedVisionAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/RedVisionAuto.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.autos;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
 import org.firstinspires.ftc.teamcode.Hardware;
 import org.firstinspires.ftc.teamcode.freesight.client.FreeSight;
 import org.firstinspires.ftc.teamcode.freesight.pipelines.FreeSightPipeline;
@@ -11,53 +12,53 @@ import org.firstinspires.ftc.teamcode.subsystems.Intake;
 
 @Autonomous
 public class RedVisionAuto extends LinearOpMode {
-  @Override
-  public void runOpMode() {
-    Hardware hardware = new Hardware(this);
-    FreeSight vision = new FreeSight(hardware, this.telemetry);
-    vision.init();
-    vision.pipe.colorState = FreeSightPipeline.Prop.PURPLE; // todo: change
-    Intake intake = new Intake(hardware, 0.7);
-    intake.stop();
-    intake.reverse();
-    InchWormBackup inchWorm = new InchWormBackup(this);
+    @Override
+    public void runOpMode() {
+        Hardware hardware = new Hardware(this);
+        FreeSight vision = new FreeSight(hardware, this.telemetry);
+        vision.init();
+        vision.pipe.colorState = FreeSightPipeline.Prop.PURPLE; // todo: change
+        Intake intake = new Intake(hardware, 0.7);
+        intake.stop();
+        intake.reverse();
+        InchWormBackup inchWorm = new InchWormBackup(this);
 
-    FreeSightPipeline.Side currentSide = null;
-    while (opModeInInit() && !opModeIsActive() && !isStopRequested()) {
-      currentSide = vision.getPosition();
+
+        FreeSightPipeline.Side currentSide = null;
+        while (opModeInInit() && !opModeIsActive() && !isStopRequested()) {
+            currentSide = vision.getPosition();
+        }
+
+        double angle = 0;
+        double yOffset = 0;
+        if (currentSide == null) currentSide = FreeSightPipeline.Side.MIDDLE;
+        switch (currentSide) {
+            case LEFT:
+                yOffset = -2;
+                angle = 60;
+                break;
+            case RIGHT:
+                angle = -30;
+                break;
+
+            case MIDDLE:
+            default:
+                yOffset = 2;
+                break;
+        }
+
+        inchWorm.moveTo(0, 27 + yOffset, Math.toRadians(angle));
+
+        double time = getRuntime() + 1;
+        intake.start();
+        while (getRuntime() < time) {
+            intake.update();
+        }
+        intake.stop();
+        intake.update(); // so cursed bruhhh
+
+        inchWorm.moveTo(-48, 5, 0); // todo: change y
+
+        vision.stop();
     }
-
-    double angle = 0;
-    double yOffset = 0;
-    if (currentSide == null)
-      currentSide = FreeSightPipeline.Side.MIDDLE;
-    switch (currentSide) {
-    case LEFT:
-      yOffset = -2;
-      angle = 60;
-      break;
-    case RIGHT:
-      angle = -30;
-      break;
-
-    case MIDDLE:
-    default:
-      yOffset = 2;
-      break;
-    }
-
-    inchWorm.moveTo(0, 27 + yOffset, Math.toRadians(angle));
-
-    double time = getRuntime() + 1;
-    intake.start();
-    while (getRuntime() < time) {
-      intake.update();
-    }
-    intake.stop();
-    intake.update(); // so cursed bruhhh
-
-    inchWorm.moveTo(-48, 5, 0); // todo: change y
-
-    vision.stop();
-  }
 }
