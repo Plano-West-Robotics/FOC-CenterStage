a7059cdc4dcc0ac04ab56258e72203d36197ac92
Yash Karandikar <nerdstep710@gmail.com>
2024-01-06 11:07:57 -0600

    idle plane launcher

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/PlaneLauncher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/PlaneLauncher.java
@@ -5,10 +5,16 @@ import org.firstinspires.ftc.teamcode.Hardware;
 public class PlaneLauncher {
     Hardware hardware;
     private static final double LAUNCH_POS = 0;
+    private static final double IDLE_POS = 0.45;
+    private static final double AIM = 0.65;
 
     public PlaneLauncher(Hardware hw) {
         hardware = hw;
 //        hardware.planeLauncher.setPosition(ENGAGED_POS);
+    }
+
+    public void idle() {
+        hardware.launcherBase.setPosition(IDLE_POS);
     }
 
     public void fire() {
@@ -16,6 +22,6 @@ public class PlaneLauncher {
     }
 
     public void aim() {
-        hardware.launcherBase.setPosition(0.65);
+        hardware.launcherBase.setPosition(AIM);
     }
 }
