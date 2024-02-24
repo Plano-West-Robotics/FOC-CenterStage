d2088ba9a9d0f2ebebde7800e42b7de6522a5256
missing <piecenotfound@gmail.com>
2024-02-23 21:12:59 -0600

    auto changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/DownstageAuto3Base.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/DownstageAuto3Base.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode.autos;
 
 import org.firstinspires.ftc.teamcode.freesight.pipelines.FreeSightPipeline;
+import org.firstinspires.ftc.teamcode.macro.RunUntil;
+import org.firstinspires.ftc.teamcode.macro.Wait;
 import org.firstinspires.ftc.teamcode.poser.Angle;
 import org.firstinspires.ftc.teamcode.poser.Distance;
 import org.firstinspires.ftc.teamcode.poser.Distance2;
@@ -11,35 +13,34 @@ public abstract class DownstageAuto3Base extends AutoBase {
         super.setup(alliance, UpOrDownStage.DOWNSTAGE);
         FreeSightPipeline.Side randomization = super.runVisionUntilStart();
 
-        poser.goTo(Distance2.inTiles(-1.5, -2.5)).run();
+//        poser.goTo(Distance2.inTiles(-1.5, -2.5)).run();
 
         Distance yOffsetAtBackdrop = Distance.ZERO;
         switch (randomization) {
             case LEFT:
-                yOffsetAtBackdrop = Distance.inInches(7);
+                yOffsetAtBackdrop = Distance.inInches(6);
                 poser.goTo(
-                        Distance.inTiles(-2),
-                        Distance.inTiles(-2).add(Distance.inInches(3))
+                        Distance.inTiles(-2).add(Distance.inInches(1)),
+                        Distance.inTiles(-2).add(Distance.inInches(6))
                 ).run();
                 break;
             case MIDDLE:
                 poser.goTo(
-                        Distance.inTiles(-1.5).sub(Distance.inInches(4.5)),
-                        Distance.inTiles(-1.5).add(Distance.inInches(4))
+                        Distance.inTiles(-2).add(Distance.inInches(6)),
+                        Distance.inTiles(-1).sub(Distance.inInches(3)),
+                        Angle.FORWARD.add(Angle.inRadians(Math.PI / 16))
                 ).run();
                 break;
             case RIGHT:
-                yOffsetAtBackdrop = Distance.inInches(-7);
-                poser.goTo(
-                        Distance.inTiles(-1.5),
-                        Distance.inTiles(-1.5)
+                yOffsetAtBackdrop = Distance.inInches(-6);
+                RunUntil.firstCompletes(
+                        Wait.millis(500),
+                        poser.goTo(Distance.inTiles(-2), Distance.inTiles(-1.5))
                 ).run();
                 poser.goTo(
+                        Distance.inTiles(-1.5).add(Distance.inInches(2.5)),
+                        Distance.inTiles(-1.5).add(Distance.inInches(2.5)),
                         Angle.FORWARD
-                ).run();
-                poser.moveBy(
-                        Distance.inInches(2.5),
-                        Distance.inInches(1)
                 ).run();
                 break;
         }
@@ -55,28 +56,36 @@ public abstract class DownstageAuto3Base extends AutoBase {
         Distance crossingYCoord = Distance.inTiles(-2.5).add(Distance.inInches(1));
         switch (randomization) {
             case LEFT:
-            case MIDDLE:
+                poser.moveBy(
+                        Distance.ZERO,
+                        Distance.inInches(-6)
+                ).run();
                 break;
+            case MIDDLE:
             case RIGHT:
                 poser.moveBy(
-                        Distance.inInches(-4),
+                        Distance.inInches(-6),
                         Distance.ZERO
                 ).run();
                 break;
         }
-        poser.goToY(crossingYCoord).run();
         poser.goTo(Angle.BACKWARD).run();
+        poser.goTo(
+                Distance.inTiles(-1.5),
+                Distance.inTiles(-2.5)
+        ).run();
         // now in position to cross to the other side of the field
 
         // configurable delay goes here, if you wish
-        sleep(6000);
 
         // migrate
+        poser.setSpeed(poser.getSpeed() / 2);
         poser.goTo(
                 Distance.inTiles(1.6),
                 crossingYCoord,
                 Angle.BACKWARD
         ).run();
+        poser.setSpeed(poser.getSpeed() * 2);
         // go in front of backdrop
         poser.goTo(
                 Distance.inTiles(1.6),
